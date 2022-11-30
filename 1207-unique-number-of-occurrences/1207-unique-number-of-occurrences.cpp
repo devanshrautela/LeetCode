cfd6574b7f51d8f95d8ec33b0a0ca7108f177a5f
Devansh Rautela <91424721+devanshrautela@users.noreply.github.com>
2022-11-30 18:01:15 +0530

    Time: 7 ms (49.07%), Space: 8.2 MB (20.75%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -1,30 +1,16 @@
-// C++ Solution
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        unordered_map<int, int> m;
-
-        // Insert array elements in a map as a key. There are no duplicate in unordered map, so for same elemts, corresponding value will be increased.
-        for(int i=0; i<arr.size(); i++){
-            m[arr[i]]++;
-        }
-
-         //Create another unordered map for storing the occurrences (freq) of array elemnts
-        unordered_map<int, int> mp;
-
-        for(auto it = m.begin(); it != m.end(); it++){
-            // Adding the number of occurrences of array elements in new map (mp)
-            mp[it->second]++;
-        }
-
-        for(auto x = mp.begin(); x != mp.end(); x++){
-            // If two or more array elements have same frequency, return false
-            if((x->second) > 1){
-                return false;
-            }
-        }
-        return true;
+     unordered_map<int,int>freq;
+     for(auto x: arr)
+     {
+         freq[x]++;
+     }
+     unordered_set<int>s;
+     for(auto x: freq)
+     {
+         s.insert(x.second);
+     }
+        return freq.size() == s.size();
     }
 };
-
-// Runtime: 0 ms, faster than 100% of C++ online submissions
